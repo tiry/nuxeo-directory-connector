70901722b4e23765353d9a773a9d9be2e8b87634
Thierry Delprat <tdelprat@nuxeo.com>
2014-04-15 20:04:15 +0200

    Remove any default config + fix filtering issue on vocabulary schema

--- src/main/java/org/nuxeo/directory/connector/json/nasa/NasaInMemoryCategoryConnector.java
+++ src/main/java/org/nuxeo/directory/connector/json/nasa/NasaInMemoryCategoryConnector.java
@@ -1,5 +1,7 @@
 package org.nuxeo.directory.connector.json.nasa;
 
+import java.util.Map;
+
 import org.codehaus.jackson.JsonNode;
 import org.nuxeo.directory.connector.json.JsonInMemoryDirectoryConnector;
 
@@ -11,4 +13,11 @@ public class NasaInMemoryCategoryConnector extends
         return responseAsJson.get("categories");
     }
 
+    @Override
+    public Map<String, Object> getEntryMap(String id) {
+        Map<String, Object> entry = super.getEntryMap(id);
+        // add the obsolete flag so that the default directory filters will work
+        entry.put("obsolete", new Long(0));
+        return entry;
+    }
 }
